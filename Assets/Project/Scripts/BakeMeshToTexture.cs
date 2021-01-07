f79f9fc99a4b1f9d8ceb4cb6295a7b00e2037d41
edom18 <edo.m18@gmail.com>
2021-01-07 23:21:00 +0900

    feat: Updated scripts.

---
+++ Assets/Project/Scripts/BakeMeshToTexture.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[RequireComponent(typeof(VisualEffect))]
+public class BakeMeshToTexture : MonoBehaviour
+{
+    [SerializeField] private GameObject _target = null;
+
+    public Texture2D BakedTexture { get; private set; }
+    public Transform Transform => _target.transform;
+
+    private SkinnedMeshRenderer _skinnedMeshRenderer = null;
+    private Mesh _mesh = null;
+    private bool _isSkinnedMesh = false;
+    private Color[] _colorBuffer = null;
+
+    public void Initialize()
+    {
+        if (_target.TryGetComponent(out MeshFilter filter))
+        {
+            _mesh = filter.mesh;
+        }
+
+        if (_target.TryGetComponent(out _skinnedMeshRenderer))
+        {
+            _isSkinnedMesh = true;
+            _mesh = new Mesh();
+            BakeMesh();
+        }
+
+        Vector3[] vertices = _mesh.vertices;
+        int count = vertices.Length;
+
+        float r = Mathf.Sqrt(count);
+        int size = (int)Mathf.Ceil(r);
+
+        _colorBuffer = new Color[size * size];
+
+        BakedTexture = new Texture2D(size, size, TextureFormat.RGBAFloat, false);
+        BakedTexture.filterMode = FilterMode.Point;
+        BakedTexture.wrapMode = TextureWrapMode.Clamp;
+
+        UpdatePositionMap();
+    }
+
+    public void UploadMeshTexture()
+    {
+        if (_isSkinnedMesh)
+        {
+            BakeMesh();
+            UpdatePositionMap();
+        }
+    }
+
+    private void BakeMesh()
+    {
+        _skinnedMeshRenderer.BakeMesh(_mesh);
+    }
+
+    private void UpdatePositionMap()
+    {
+        int idx = 0;
+        foreach (Vector3 vert in _mesh.vertices)
+        {
+            _colorBuffer[idx] = VectorToColor(vert);
+            idx++;
+        }
+
+        BakedTexture.SetPixels(_colorBuffer);
+        BakedTexture.Apply();
+    }
+
+    private Color VectorToColor(Vector3 v)
+    {
+        return new Color(v.x, v.y, v.z);
+    }
+}
--- Assets/Project/Scripts/MeshPositionMapBinder.cs
+++ Assets/Project/Scripts/MeshPositionMapBinder.cs
@@ -8,84 +8,36 @@ using UnityEngine.VFX;
 [RequireComponent(typeof(VisualEffect))]
 public class MeshPositionMapBinder : MonoBehaviour
 {
-    [SerializeField] private GameObject _target = null;
-    [SerializeField] private int _index = 1;
+    [SerializeField] private BakeMeshToTexture[] _bakers = null;
 
     private VisualEffect _vfx = null;
-    private SkinnedMeshRenderer _skinnedMeshRenderer = null;
-    private MeshFilter _meshFilter = null;
-
-    private Mesh _mesh = null;
-    private Texture2D _bakedTex = null;
+    private int _index = -1;
 
     private void Awake()
     {
         _vfx = GetComponent<VisualEffect>();
 
-        if (_target.TryGetComponent(out MeshFilter filter))
+        foreach (var b in _bakers)
         {
-            _mesh = filter.mesh;
+            b.Initialize();
         }
 
-        if (_target.TryGetComponent(out _skinnedMeshRenderer))
-        {
-            _mesh = new Mesh();
-        }
+        Change();
     }
 
-    private void Update()
+    private void Change()
     {
-        UploadMeshTexture();
+        _index = (_index + 1) % _bakers.Length;
+        _vfx.SetTexture($"PositionMap", _bakers[_index].BakedTexture);
+        _vfx.SetInt($"Size", _bakers[_index].BakedTexture.width);
+        _vfx.SetMatrix4x4($"VolumeTransform", _bakers[_index].Transform.localToWorldMatrix);
     }
 
-    private void UploadMeshTexture()
+    private void OnGUI()
     {
-        UpdateMesh();
-        UpdatePositionMap();
-
-        _vfx.SetTexture($"PositionMap{_index}", _bakedTex);
-        _vfx.SetInt($"Size{_index}", _bakedTex.width);
-        _vfx.SetMatrix4x4($"VolumeTransform{_index}", _target.transform.localToWorldMatrix);
-    }
-
-    private void UpdateMesh()
-    {
-        if (_skinnedMeshRenderer != null)
+        if (GUI.Button(new Rect(10, 10, 130, 30), "Change"))
         {
-            _skinnedMeshRenderer.BakeMesh(_mesh);
+            Change();
         }
-    }
-
-    private void UpdatePositionMap()
-    {
-        Vector3[] vertices = _mesh.vertices;
-        int count = vertices.Length;
-
-        float r = Mathf.Sqrt(count);
-        var width = (int)Mathf.Ceil(r);
-        var height = width;
-
-        var positions = vertices.Select(vtx => new Color(vtx.x, vtx.y, vtx.z));
-
-        if (_bakedTex == null || _bakedTex.width != width || _bakedTex.height != height)
-        {
-            Destroy(_bakedTex);
-            
-            _bakedTex = new Texture2D(width, height, TextureFormat.RGBAFloat, false);
-            _bakedTex.filterMode = FilterMode.Point;
-            _bakedTex.wrapMode = TextureWrapMode.Clamp;
-        }
-
-        var buf = new Color[width * height];
-
-        var idx = 0;
-        foreach (var color in positions)
-        {
-            buf[idx] = color;
-            idx++;
-        }
-
-        _bakedTex.SetPixels(buf);
-        _bakedTex.Apply();
     }
 }
