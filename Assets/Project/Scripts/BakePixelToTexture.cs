9935472bcabdf8304dd1c2c52227b3431416ebce
edom18 <edo.m18@gmai.com>
2021-01-08 10:25:24 +0900

    feat: Added assets for moving texture particle.

---
+++ Assets/Project/Scripts/BakePixelToTexture.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class BakePixelToTexture : MonoBehaviour
+{
+    [SerializeField] private Texture2D _texture = null;
+    [SerializeField] private float _scale = 0.01f;
+    [SerializeField] private int _divide = 2;
+
+    public Texture2D PositionMap { get; private set; }
+    public Texture2D ColorMap { get; private set; }
+
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+
+    public void Initialize()
+    {
+        Width = _texture.width / _divide;
+        Height = _texture.height / _divide;
+
+        ColorMap = new Texture2D(Width, Height);
+        Graphics.ConvertTexture(_texture, ColorMap);
+        
+        PositionMap = new Texture2D(Width, Height, TextureFormat.RGBAFloat, false);
+        PositionMap.filterMode = FilterMode.Point;
+        PositionMap.wrapMode = TextureWrapMode.Clamp;
+
+        UpdatePositionMap();
+    }
+
+    private void UpdatePositionMap()
+    {
+        Color[] buffer = new Color[Width * Height];
+        
+        for (int y = 0; y < Height; y++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                Vector3 v = new Vector3(x * _scale, y * _scale, 0);
+                buffer[y * Width + x] = VectorToColor(v);
+            }
+        }
+
+        PositionMap.SetPixels(buffer);
+        PositionMap.Apply();
+    }
+
+    private Color VectorToColor(Vector3 v)
+    {
+        return new Color(v.x, v.y, v.z);
+    }
+}
---
+++ Assets/Project/Scripts/TexturePositionMapBinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[RequireComponent(typeof(VisualEffect))]
+public class TexturePositionMapBinder : MonoBehaviour
+{
+    [SerializeField] private BakePixelToTexture _baker = null;
+
+    private VisualEffect _vfx = null;
+
+    private void Awake()
+    {
+        _vfx = GetComponent<VisualEffect>();
+        _baker.Initialize();
+
+        _vfx.SetTexture("PositionMap", _baker.PositionMap);
+        _vfx.SetTexture("ColorMap", _baker.ColorMap);
+        _vfx.SetInt("Width", _baker.Width);
+        _vfx.SetInt("Height", _baker.Height);
+    }
+}
