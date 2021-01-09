fff855498fe9c41eab64173613012cc95974ffc6
edom18 <edo.m18@gmail.com>
2021-01-09 11:51:10 +0900

    feat: Removed unused "using"s.

--- Assets/Project/Scripts/BakeMeshToTexture.cs
+++ Assets/Project/Scripts/BakeMeshToTexture.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.VFX;
 
 [RequireComponent(typeof(VisualEffect))]
