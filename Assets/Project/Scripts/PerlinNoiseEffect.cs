ab0292a280c4fb66f13dc04c32aa9901ccea6792
edom18 <edo.m18@gmail.com>
2021-01-09 20:00:53 +0900

    feat: Added a new effect.

---
+++ Assets/Project/Scripts/PerlinNoiseEffect.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[RequireComponent(typeof(VisualEffect))]
+public class PerlinNoiseEffect : MonoBehaviour
+{
+    [SerializeField] private BakePixelToTexture _baker = null;
+    [SerializeField] private float _duration = 2f;
+
+    private VisualEffect _vfx = null;
+
+    private void Awake()
+    {
+        _vfx = GetComponent<VisualEffect>();
+        _baker.Initialize();
+
+        _vfx.SetTexture("PositionMap", _baker.PositionMap);
+        _vfx.SetTexture("ColorMap", _baker.ColorMap);
+        _vfx.SetInt("Width", _baker.Width);
+        _vfx.SetInt("Height", _baker.Height);
+        
+        ResetParticle();
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 130, 30), "Start"))
+        {
+            StartNoise();
+        }
+        
+        if (GUI.Button(new Rect(10, 50, 130, 30), "Reset"))
+        {
+            ResetParticle();
+        }
+    }
+
+    private void ResetParticle()
+    {
+        _vfx.SetFloat("Progress", 0);
+        _vfx.SendEvent("OnPlay");
+    }
+
+    private void StartNoise()
+    {
+        StartCoroutine(StartNoiseCoroutine());
+    }
+
+    private IEnumerator StartNoiseCoroutine()
+    {
+        float time = 0;
+        while (time <= _duration)
+        {
+            time += Time.deltaTime;
+            float t = time / _duration;
+            _vfx.SetFloat("Progress", t);
+            yield return null;
+        }
+    }
+}
